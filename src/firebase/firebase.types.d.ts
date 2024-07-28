6755c28200a4539eca3d38d133e4b120763a5649
antoineNC <antoineney@hotmail.fr>
2024-07-29 00:07:05 +0200

    delete some firebase types

--- src/firebase/firebase.types.d.ts
+++ src/firebase/firebase.types.d.ts
@@ -1,52 +1,5 @@
 import { DocumentReference, Timestamp } from "firebase/firestore";
 
-type fb_Student = {
-  mail: string;
-  role: string;
-  lastName: string;
-  firstName: string;
-  adhesion: string[];
-};
-
-type fb_Office = {
-  mail: string;
-  role: string;
-  name: string;
-  acronym: string;
-  description: string;
-  role: string;
-  members: {
-    idStudent: string;
-    nameStudent: string;
-    idRole: string;
-    nameRole: string;
-  }[];
-  clubs: string[];
-  partnerships: string[];
-};
-
-type fb_Post = {
-  title: string;
-  description: string;
-  editor: string;
-  image: string;
-  tags: Array<string>;
-  createdAt: Timestamp;
-  visibleCal: boolean;
-  date: {
-    start?: Timestamp;
-    end?: Timestamp;
-  };
-};
-
-type fb_Club = {
-  name: string;
-  description: string;
-  contact: string;
-  logo: string;
-  office: string;
-};
-
 type fb_Partnership = {
   name: string;
   description: string;
