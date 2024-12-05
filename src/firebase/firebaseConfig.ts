6a9fc44bd415d5c457ffb17e3a1761c195346605
antoineNC <antoineney@hotmail.fr>
2024-12-05 21:49:28 +0100

    add env variables

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -1,20 +1,15 @@
+import { config } from "@config";
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDcNbKtopB7Dxp2nGcYFlDiQ4TukEpoMPc",
-  authDomain: "poulpappv2.firebaseapp.com",
-  projectId: "poulpappv2",
-  storageBucket: "poulpappv2.appspot.com",
-  messagingSenderId: "427196722560",
-  appId: "1:427196722560:web:d44eb4a0390dee45dc2565",
-  measurementId: "G-2XW9TZKH8R",
+  apiKey: config.firebase.apiKey,
+  authDomain: config.firebase.authDomain,
+  projectId: config.firebase.projectId,
+  storageBucket: config.firebase.storageBucket,
+  messagingSenderId: config.firebase.messagingSenderId,
+  appId: config.firebase.appId,
+  measurementId: config.firebase.measurementId,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 export default app;
