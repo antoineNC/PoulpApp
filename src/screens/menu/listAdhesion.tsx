d9bac2f0dccde473ceb467e53c9286a93790725d
antoineNC <antoineney@hotmail.fr>
2024-12-14 14:28:35 +0100

    fix/adhesions

--- src/screens/menu/listAdhesion.tsx
+++ src/screens/menu/listAdhesion.tsx
@@ -1,15 +1,23 @@
 import { $officeStore } from "@context/officeStore";
 import { $sessionStore } from "@context/sessionStore";
+import { $studentStore } from "@context/studentStore";
 import { Container } from "@styledComponents";
 import { useStoreMap, useUnit } from "effector-react";
 import { FlatList } from "react-native";
 import { Card } from "react-native-paper";
 
 export default function ListAdhesion() {
-  const { user, student } = useUnit($sessionStore);
+  const { userId } = useUnit($sessionStore);
+  const student = useStoreMap({
+    store: $studentStore,
+    keys: [userId],
+    fn: (studentList) => {
+      return studentList.find((value) => value.id === userId);
+    },
+  });
   const listOffice = useStoreMap({
     store: $officeStore,
-    keys: [user.id],
+    keys: [userId],
     fn: (officeStore) => {
       const list = officeStore.officeList.filter((el) =>
         student?.adhesion?.includes(el.id)
