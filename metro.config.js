37c6ffb76a3b69bc73c2c9bfbf5f32ac929a9257
antoineNC <antoineney@hotmail.fr>
2025-04-29 16:29:53 +0200

    add config for rn reanimated

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,10 @@
 const { getDefaultConfig } = require("@expo/metro-config");
+const {
+  wrapWithReanimatedMetroConfig,
+} = require("react-native-reanimated/metro-config");
 
 // eslint-disable-next-line no-undef
 const defaultConfig = getDefaultConfig(__dirname);
 defaultConfig.resolver.sourceExts.push("cjs");
 
-module.exports = defaultConfig;
+module.exports = wrapWithReanimatedMetroConfig(defaultConfig);
